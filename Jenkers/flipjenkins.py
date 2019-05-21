0fcd83ab7b9ed84d9a3ac22c0a41925d72cacbea
Andrew <andrewp.wichmann@gmail.com>
2019-05-20 23:09:48 -0500

    only flip once

--- Jenkers/flipjenkins.py
+++ Jenkers/flipjenkins.py
@@ -12,12 +12,11 @@ wiringpi.pwmSetRange(2000)
 
 delay_period = 0.01
 
-while True:
-    for pulse in range(50, 250, 1):
-        print("rotating 1")
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
-    for pulse in range(250, 50, -1):
-        print("rotating -1")
-        wiringpi.pwmWrite(18, pulse)
-        time.sleep(delay_period)
+for pulse in range(50, 250, 1):
+    print("rotating 1")
+    wiringpi.pwmWrite(18, pulse)
+    time.sleep(delay_period)
+for pulse in range(250, 50, -1):
+    print("rotating -1")
+    wiringpi.pwmWrite(18, pulse)
+    time.sleep(delay_period)
