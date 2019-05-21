cb2da34fe3c42bccb92f24401df80d916ffa769d
Andrew <andrewp.wichmann@gmail.com>
2019-05-20 22:58:22 -0500

    flip that jenkers

--- Jenkers/flipjenkins.py
+++ Jenkers/flipjenkins.py
@@ -1,2 +1,23 @@
-with open('testfile', 'w') as f:
-    f.writelines('hello')
+import time
+import wiringpi
+
+wiringpi.wiringPiSetupGpio()
+
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+
+delay_period = 0.01
+
+while True:
+    for pulse in range(50, 250, 1):
+        print("rotating 1")
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
+    for pulse in range(250, 50, -1):
+        print("rotating -1")
+        wiringpi.pwmWrite(18, pulse)
+        time.sleep(delay_period)
